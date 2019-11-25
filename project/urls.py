e24893167bbdff0b8ccd3efa9edfc96a91dae700
hajam09 <hajam09@yahoo.com>
2019-11-25 14:25:49 +0000

    changed urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,12 @@
-from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.http import HttpResponse
 
-from welcome.views import index, health
+def health(request):
+    return HttpResponse('health')
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
+    path('health/', health, name="health"),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
