70809a51f2e3c0d71762858698768e5f4b2e8020
hajam09 <hajam09@yahoo.com>
2019-11-25 15:26:27 +0000

    not final 2

--- project/settings.py
+++ project/settings.py
@@ -83,7 +83,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 from . import database
 
 DATABASES = {
-    'default': database.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
