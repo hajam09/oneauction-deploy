aa90c492fb2b5d2295c3171a2d6be78651bc06d5
hajam09 <hajam09@yahoo.com>
2019-11-25 14:03:56 +0000

    added

--- auctionapp/settings.py
+++ auctionapp/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'xthx9h_wxp)i9=b&!*g=14fgalrsi-1_-)^0*_yo+b58)v=431'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
@@ -37,13 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'mainapp',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "qmwebprog2019@gmail.com"
+EMAIL_HOST_USER = "qmwebprog201django>=1.11,<1.12
+django-debug-toolbar==1.8
+gunicorn==19.4.5
+psycopg2==2.7.3.1
+whitenoise==3.3.19@gmail.com"
 EMAIL_HOST_PASSWORD = "OneAuction"
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Epicups Team <admin@epicups.com>'
@@ -56,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'auctionapp.urls'
@@ -142,3 +149,5 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
