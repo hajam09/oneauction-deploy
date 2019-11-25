430754e74c69dbe80819fddb72aa9154ca1c98a2
hajam09 <hajam09@yahoo.com>
2019-11-25 14:57:56 +0000

    advd

---
+++ project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse('health')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('health/', health, name="health"),
+]
