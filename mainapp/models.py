efa79c4f8da4ea5a1e00a215579c1d112cd7ab6c
hajam09 <hajam09@yahoo.com>
2019-11-26 18:01:53 +0000

    changed the relationship

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class CustomerProfile(models.Model):
-	userid = models.OneToOneField(User, on_delete=models.CASCADE)#Onetoone
+	userid = models.ForeignKey(User, on_delete=models.CASCADE)#Onetoone
 	birthDate = models.DateField()
 	email = models.CharField(max_length=1000, default="")
 
